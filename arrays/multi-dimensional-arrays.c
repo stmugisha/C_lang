7436cd2abe7c2f2ce8bdc3c56355251b5e090809
steph-en-m <mstevejean@gmail.com>
2020-01-02 23:46:24 +0300

    matrix addition

--- arrays/multi-dimensional-arrays.c
+++ arrays/multi-dimensional-arrays.c
@@ -1,17 +1,20 @@
 //Author: Stephen Mugisha
 
 /*
-Multi-dimensional arrays: Can store elements of more than
-one dimension. e.g matrices 
+Multi-dimensional arrays:
+A program to add two matrices
 */
 
 #include <stdio.h>
 
 int main()
 {
-    int m[2][2] = {1, 0, 0, 1};
-    int n[2][2] = {1, 1, 1, 1};
+    int m[2][2] = {{1, 0}, {0, 1}};
+    int n[2][2] = {{1, 1}, {1, 1}};
+    
+    int j[3][2] = {{2, 2}, {1, 3}};
+    int k[1][3] = {{4, 5}, {0, 6}};
 
-    printf("%d\n", &m[1]);
+    printf("%d\n", *m[1]);
     printf("Total: %d\n", *n[1]+*m[0]);
 }
