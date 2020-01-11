5e76e4d1ba86ed3a27dbb613f2b4e66f8811d1e3
steph-en-m <mstevejean@gmail.com>
2020-01-12 00:18:50 +0300

    string case conversion

--- strings/lowerToUpper.c
+++ strings/lowerToUpper.c
@@ -6,3 +6,25 @@
 #include <ctype.h>
 
 void toUppercase(char *sPtr);
+
+int main()
+{
+    char string[] = "Agent of Shield..";
+    printf("Input string:\n%s\n", string);
+    toUppercase(string);
+    printf("Formatted string:\n%s\n", string);
+
+    return 0;
+}
+
+void toUppercase(char *sPtr)
+{
+    while(*sPtr != '\0')
+    {
+        if(islower(*sPtr))
+        {
+            *sPtr = toupper(*sPtr);
+        }
+        ++sPtr;
+    }
+}
