faa014bf910120143898a5c0d8b0fbadd5cd456b
steph-en-m <mstevejean@gmail.com>
2020-01-16 20:00:11 +0300

    making a copy of a string

---
+++ strings/strCopying.c
@@ -0,0 +1,20 @@
+/* Creating a copy of a string */
+
+#include <stdio.h>
+#define SIZE 17
+
+void copy(const char *str1, char *str2);
+
+int main()
+{
+    char *string1 = "Agent of Shield.";
+    char _copy[SIZE];
+
+    copy(string1, _copy);
+    printf("Copied string:\n %s\n", _copy);
+}
+
+void copy(const char *str1, char *str2)
+{
+    for(; (*str2 = *str1) != '\0'; ++str2, ++str1){;}
+}
